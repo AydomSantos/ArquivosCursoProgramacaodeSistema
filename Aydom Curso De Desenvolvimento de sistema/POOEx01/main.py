dd7419d10a2a0af4950e5d7a3247063525478053
Aydom Aparecido dos Santos <101830296+AydomSantos@users.noreply.github.com>
2024-05-22 15:00:54 -0300

    Add files via upload

--- Aydom Curso De Desenvolvimento de sistema/POOEx01/main.py
+++ Aydom Curso De Desenvolvimento de sistema/POOEx01/main.py
@@ -26,9 +26,35 @@ class Pessoa:
 
 p1 = Pessoa("Aydom", 23, 60, 1.70)
 
+# Resposta
 
 
+class Pessoa:
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
 
+    def Envelhecer(self):
+        self.idade += 1
+        if self.idade <= 21:
+            self.Crecer(0.5)
+
+    def Engorda(self, novoPeso):
+        self.peso += novoPeso
+    def Enmagrecer(self, novoPeso):
+        self.peso -= novoPeso
+
+    def Crecer(self, novaAltura):
+        self.altura += novaAltura
+
+    def __str__(self):
+        return f"Nome: {self.nome}\nIdade: {self.idade}\nPeso: {self.peso}\nAltura: {self.altura}"
+
+
+pessoaUm = Pessoa("Aydom", 23, 60, 1.70)
+print(pessoaUm)
 
 
     
